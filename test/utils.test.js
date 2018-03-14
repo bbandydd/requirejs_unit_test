405651fd21fcec49f2c748594f1756a4ea72c133
Andy Tsai <bbandydd@gmail.com>
2018-03-15 02:02:02 +0800

    add utils.test.js

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,5 +1,18 @@
 require('amd-loader');
+const chai = require('chai');
+const expect = chai.expect;
 
 define(['../js/utils.js'], (utils) => {
-    console.log(utils);
+    describe('utils', () => {
+        it('should get data', () => {
+            const data = {
+                data: [
+                    { id: 1, text: '111'},
+                    { id: 2, text: '222'},
+                ]
+            };
+
+            expect(utils.getData()).to.deep.equal(data);
+        });
+    });
 });
